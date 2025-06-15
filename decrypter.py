93ed213a581232855689d3f22aca02f0253c98ab
abeltei <110141340+abeltei@users.noreply.github.com>
2025-06-15 14:33:25 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,24 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
+# Abrir o arquivo criptografado
 file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+with open(file_name, "rb") as file:
+    file_data = file.read()
 
-## chave para descriptografia
-key = b"testeransomwares"
+# Chave para descriptografia (deve ser exatamente a mesma da criptografia)
+key = b"testeransomwares"[:16]  # Pegando os primeiros 16 bytes
+
+# Descriptografar os dados
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+# Remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+# Criar o arquivo descriptografado
 new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+with open(new_file, "wb") as file:
+    file.write(decrypt_data)
+
+print(f"Arquivo '{file_name}' descriptografado com sucesso para '{new_file}'!")
